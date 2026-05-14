ec9df24bdc5a9ec4dfb36020d900a751b50ed872
Dhevansh-python <dhev.reddy@icloud.com>
2026-05-14 18:06:16 +0100

    Add files via upload

---
+++ homework10.py
@@ -0,0 +1,14 @@
+num1=int(input("Enter your 1st number: "))
+num2=int(input("Enter your 2nd number: "))
+num3=int(input("Enter your 3rd number: "))
+interchange=num1
+
+num1=num2
+num2=num3
+num3=interchange
+
+print("Your swaped number1= ", (num1))
+print("Your swaped number2= ", (num2))
+print("Your swaped number3= ", (num3))
+
+
