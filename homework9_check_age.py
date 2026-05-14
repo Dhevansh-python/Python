f53c08362f902347f2e8578c22bb4eeb375d2b87
Dhevansh-python <dhev.reddy@icloud.com>
2026-05-14 18:14:32 +0100

    Add files via upload

---
+++ homework9_check_age.py
@@ -0,0 +1,7 @@
+age=int(input("Enter your age because we need to check to see if you are old enough to go to raj's class : "))
+
+if age>=10 and age<=20:
+    print("You can join raj's class")
+
+else:
+    print("You cannot join raj's class")
