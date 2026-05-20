9e8a91d63d889de00fb6a3cc5fa5ec2e5274a583
Dhevansh-python <dhev.reddy@icloud.com>
2026-05-20 19:24:41 +0100

    Add files via upload

---
+++ power calculator.py
@@ -0,0 +1,17 @@
+x=int(input("Enter a number to square root: "))
+y=int(input("Enter a number to square root: "))
+
+
+power=x
+for i in range(1, y):
+    power=power*x
+
+
+print("the value of", x, "to the power of", y, "is ",power)
+
+
+
+
+
+
+
