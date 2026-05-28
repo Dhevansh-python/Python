fde033cc706c5a6fd2842eba590728120c5a85d4
Dhevansh-python <dhev.reddy@icloud.com>
2026-05-28 20:20:48 +0100

    Add files via upload

---
+++ age_vscode.py
@@ -0,0 +1,10 @@
+age=int(input("Enter an age between 10-20 to enrol in raj's class: "))
+
+if age>=10 and age<20:
+    print("you can enrol in raj's class")
+
+elif age>10 and age>20:
+    print("you cannot enrol in raj's class")
+
+else:
+    print("please enter an age between 10-20 ")
