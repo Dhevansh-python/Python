aede89e1e9e2926daeb08421c4747a118bb5fe09
Dhevansh-python <dhev.reddy@icloud.com>
2026-06-01 17:20:04 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+number=int(input("Enter a number for me to guess between 1-50: "))
+attemps=0
+
+while attemps<=5:
+    if attemps>5:
+        print("You have ran out of attemps")
+    else:
+        ("it is time to tell hint")
