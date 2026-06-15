9142a3d44c7686055d8d0880dde39498771f874a
Dhevansh-python <dhev.reddy@icloud.com>
2026-06-15 17:18:07 +0100

    Add files via upload

---
+++ function.py
@@ -0,0 +1,9 @@
+def monkey(num1,num2,num3,num4):
+    res=(num1+num2-num3+num4)
+    print(res)
+argument1=int(input("Enter a number: "))
+argument2=int(input("Enter 2nd number: "))
+argument3=int(input("Enter 3rd number: "))
+argument4=int(input("Enter 4th number: "))
+
+monkey(argument1,argument2,argument3,argument4)
