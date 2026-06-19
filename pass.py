91df9efc9dd394beab30637c4786adf45c038a84
Dhevansh-python <dhev.reddy@icloud.com>
2026-06-19 15:11:50 +0100

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,6 @@
+for questions in ["3x6+3451937", "2x7+3", "4x6+65738", "5x8+3", "7x1", "4x7+829130"]:
+    if questions=="3x6+3451937" or questions=="4x6+65738" or questions=="4x7+829130":
+        pass
+
+    else:
+        print("I know that question")
