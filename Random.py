8e49acd8a93a496763644779b44580d50bf2a281
Dhevansh-python <dhev.reddy@icloud.com>
2026-07-06 17:11:57 +0100

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,30 @@
+import random
+
+score=random.randint(0,20)
+score1=random.random()
+print(score,score1)
+
+
+choco=['dairy milk', 'kitkat', 'kinder', 'cream egg', 'm&ms']
+pick=random.choice(choco)
+num=random.choice("07450233422")
+print(f"You got {num} {pick} chocolates")
+
+name=random.choice("Dhevansh")
+print(name)
+
+name=(input("Enter your name: "))
+dob=(input("Enter you dob: "))
+username=random.choice(name)+random.choice(name)+random.choice(name)+random.choice(dob)+random.choice(dob)
+print("your name is", username)
+
+
+
+
+
+
+
+
+
+
+
